afe70b139cc9fa98bac08b3fbd2cda7228d93ab5
NateGithub9 <nathaniel.akingbade@gmail.com>
2024-06-14 12:42:36 +0200

    exo comparatif mot user et mot app

---
+++ Openclassroom 1/script.js
@@ -0,0 +1,9 @@
+const motApplication = "Codepen"
+
+let mot_user = prompt("Veuillez entrer le mot : " + motApplication);
+if (motApplication === mot_user) {
+    alert("Bravo!");
+    
+} else {
+    alert("Erreur de frappe!");
+}
